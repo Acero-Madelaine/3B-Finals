73042db083e273a7fd24b0f05a23a2fb5729e9cd
S-chan <ragingpatata123@gmail.com>
2024-06-30 20:58:32 +0800

    this is for user 2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubjectController;
+
+Route::get('/students/{id}/subjects', [SubjectController::class, 'index2']);
+Route::post('/students/{id}/subjects', [SubjectController::class, 'store2']);
+Route::get('/students/{id}/subjects/{subject_id}', [SubjectController::class, 'show2']);
+Route::patch('/students/{id}/subjects/{subject_id}', [SubjectController::class, 'update2']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
